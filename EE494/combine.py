b7fc73796669df88077f768cba6616c8cc0f2d16
kepler42 <36304051+kepler42@users.noreply.github.com>
2021-04-11 18:09:14 -0400

    Add files via upload

---
+++ EE494/combine.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np 
+import os, glob
+
+ 
+list_of_files = glob.glob(os.path.join("/home/kepler42/EE494/Dataset_EE_lounge/", "EE494_data_*.csv"))
+
+df_from_each_file = (pd.read_csv(f, sep=',',header=None) for f in list_of_files)
+df_merged   = pd.concat(df_from_each_file, ignore_index=True)
+df_merged.to_csv( "merged.csv")
---
+++ EE494/load_data.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np 
+
+
+f = "/home/kepler42/EE494/merged.csv"
+df = pd.read_csv(f,sep=',')
